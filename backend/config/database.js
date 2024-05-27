d1f00b6ec5e9c7e0e5c9ddba89a3978d53b7d7eb
MarcoGiacoppo <marcogiacoppo19@gmail.com>
2024-05-27 23:44:09 +1000

    cancel vercel

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,14 @@
 import mysql from "mysql2";
 
-// create the connection to database using environment variables
+// create the connection to database
+
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "db_restaurant"
 });
+
 
 db.connect(error => {
   if (error) throw error;
